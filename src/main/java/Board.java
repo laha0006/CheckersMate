9d3ed951dc63a199389e5c4705137c41f0c7b942
Dosei-desu <krispauly@hotmail.com>
2025-05-05 11:54:09 +0200

    board stuff

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -21,6 +21,30 @@ public class Board {
         return boardState;
     }
 
+    public static String moveGuide(){
+        return """
+                +-------------------------+
+                | -- 00 -- 01 -- 02 -- 03 |
+                | 04 -- 05 -- 06 -- 07 -- |
+                | -- 08 -- 09 -- 10 -- 11 |
+                | 12 -- 13 -- 14 -- 15 -- |
+                | -- 16 -- 17 -- 18 -- 19 |
+                | 20 -- 21 -- 22 -- 23 -- |
+                | -- 24 -- 25 -- 26 -- 27 |
+                | 28 -- 29 -- 30 -- 31 -- |
+                +-------------------------+
+                """;
+    }
+
+    public static String printBoard(){
+        StringBuilder string = new StringBuilder();
+
+        string.append("+-------------------------+");
+        string.append("| -- "+);
+
+        return string;
+    }
+
     public int getBlack() {
         return black;
     }
